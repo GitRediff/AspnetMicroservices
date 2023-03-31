19c4b867d7aea7d54d479a2649d062dfeee83d9e
Prasad <kooldreamz@outlook.com>
2023-03-31 16:03:40 +0530

    Ocelot.Json(local/develoment Envs) and its config in program.cs

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -18,13 +18,20 @@ namespace OcelotApiGw
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                // Below for which ocelot version json file to pickup ---> ocelot.Development.json or ocelot.Local.json
+                // this will add config file when application started [AddJSonFile()]
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile($"ocelot.{ hostingContext.HostingEnvironment.EnvironmentName }.json", true, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 })
+                // below for logging on console and debug
                 .ConfigureLogging((hostingContext, loggingBuilder) =>
                 {
-                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));  
                     loggingBuilder.AddConsole();
                     loggingBuilder.AddDebug(); 
                 });
